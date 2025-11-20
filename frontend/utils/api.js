0bae2236ac3d3e497828e81b24924d2d1cdf8693
snowleaopardxt <gunjit15@gmail.com>
2025-11-21 02:15:25 +0530

    folder structure created

---
+++ frontend/utils/api.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+
+const api = axios.create({
+baseURL: process.env.VITE_API_URL || 'http://localhost:3000/api',
+headers: { 'Content-Type': 'application/json' }
+});
+
+module.exports = api;
